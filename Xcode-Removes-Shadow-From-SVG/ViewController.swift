0b53e812179ff0a8345328012cd5c02ae3a3db32
Luisa Castaño <lcastano@kayak.com>
2022-03-25 14:15:53 +0100

    Initial Commit

---
+++ Xcode-Removes-Shadow-From-SVG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode-Removes-Shadow-From-SVG
+//
+//  Created by Luisa Fernanda Castaño on 25.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
